a6347a3c0f7d59822c333eecab097772bab55458
Karan Vyas <karanvyas2907@gmail.com>
2022-03-25 22:53:50 +0530

    added styled components to About sections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+//Global styles
+import GlobalStyle from "./Components/GlobalStyle";
+
+// import pages 
+import AboutUs from "./Pages/AboutUs";
+
+
 function App() {
   return (
     <div className="App">
-    
+      <GlobalStyle/>
+      <AboutUs/>
     </div>
   );
 }
---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AboutSection from "../Components/AboutSection";
+import FaqSection from "../Components/FaqSection";
+import ServicesSection from "../Components/ServicesSection";
+
+const AboutUs = () => {
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
+    </>
+  );
+};
+
+export default AboutUs;
