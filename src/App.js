56416fc64035750cbe5e5759bcd295c655995788
Karan Vyas <karanvyas2907@gmail.com>
2022-03-26 22:56:41 +0530

    added movie details

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import React from "react";
 //Global styles
 import GlobalStyle from "./Components/GlobalStyle";
 
-// import pages 
+// import pages
 import AboutUs from "./Pages/AboutUs";
+import Nav from "./Components/Nav";
+import ContactUs from "./Pages/ContactUs";
+import OurWork from "./Pages/OurWork";
+import MovieDetail from "./Pages/MovieDetails";
 
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle/>
-      <AboutUs/>
+      <GlobalStyle />
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <AboutUs />
+        </Route>
+        <Route exact path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
