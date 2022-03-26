ba365dccef224bf47a44a64ae3b319e4fdbc08a9
Karan Vyas <karanvyas2907@gmail.com>
2022-03-26 23:15:19 +0530

    Added framework motion to aboutus

--- src/Components/AboutSection.js
+++ src/Components/AboutSection.js
@@ -1,27 +1,44 @@
 import React from "react";
 import home1 from "../img/home1.png";
+import { About, Description, Image, Hide } from "../styles";
 
-// Styled
-import styled from "styled-components";
-import { About,Description, Image, Hide } from "../styles";
+//Framer Motion
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+  const titleAnim = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 2 } },
+  };
+
+  const container = {
+    hidden: { x: 100 },
+    show: { x: 0, transition: { duration: 0.75, ease: "easeOut", staggerChildren: 1, when: "beforeChildren"} },
+  };
+
   return (
     <About>
       <Description className="description">
-        <div className="title">
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="title"
+        >
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnim}> 
+              We work to make
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true</h2>
+            <motion.h2 variants={titleAnim}>true</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills
@@ -34,37 +51,5 @@ const AboutSection = () => {
     </About>
   );
 };
-
-// Styled components
-// const About = styled.div`
-//   min-height: 90vh;
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   padding: 5rem 10rem;
-//   color: white;
-// `;
-
-// const Description = styled.div`
-//   flex: 1;
-//   padding-right: 5rem;
-//   h2 {
-//     font-weight: lighter;
-//   }
-// `;
-
-// const Image = styled.div`
-//   flex: 1;
-//   overflow: hidden;
-//   img{
-//       width: 100%;
-//       height: 80vh;
-//       object-fit: cover;
-//   }
-// `;
-
-// const Hide = styled.div`
-//     overflow: hidden;
-// `
 
 export default AboutSection;
